09b40408bb7d6bf117eef4c60cd97af55a942842
Yeva Vakhtina <yeva.vakhtina1@gmail.com>
2022-02-15 13:30:50 +0200

    Higher Order Components

---
+++ src/HOCs/withToggler.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react"
+
+class Toggler extends Component {
+    state = {
+        on: this.props.defaultOnValue
+    }
+    
+    toggle = () => {
+        this.setState(prevState => {
+            return {
+                on: !prevState.on
+            }
+        });
+    }
+    
+    render() {
+        const {component: CreatedComponent, defaultOnValue, ...props} = this.props;
+        return (
+            <CreatedComponent on={this.state.on} toggle={this.toggle} {...props} />
+        )
+    }
+}
+
+export function withToggler(component, optionsObj) {
+    return function(props) {
+        return (
+            <Toggler component={component} defaultOnValue={optionsObj.defaultOnValue} {...props}/>
+        )
+    }
+}
