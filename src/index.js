0db2cd4c8aaa860409f8e998dff47ec669ff63b3
Yeva Vakhtina <yeva.vakhtina1@gmail.com>
2022-03-10 17:17:59 +0200

    signup page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import { app } from './lib/firebase';
+import { db } from './lib/firebase';
 import FirebaseContext from './context/firebase';
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={{ app }}>
+    <FirebaseContext.Provider value={{ db }}>
         <React.StrictMode>
             <App />
         </React.StrictMode>
